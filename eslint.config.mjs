ce1563f3c6adddd4a64401762046d61cf20e0db1
ferriyusra <ferisakuragi@gmail.com>
2025-07-19 22:19:43 +0700

    feat: setup code base with darkmode

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,13 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+eslintConfig.push({
+  rules: {
+    'react-hooks/exhautive-deps': 'off', // Disable exhaustive-deps rule for react-hookss
+    'react-jsx-key': 'off', // Disable react-jsx-key rule
+    '@typescript-eslint/no-explicit-any': 'off', // Disable no-explicit-any rule
+    '@typescript-eslint/no-unused-vars': 'off', // Disable no-unused-vars rules
+  },
+})
+
 export default eslintConfig;
