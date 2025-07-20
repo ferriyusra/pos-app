c802efe6d13106c03bc5ead819897d9d0f31c034
ferriyusra <ferisakuragi@gmail.com>
2025-07-20 19:32:04 +0700

    feat: setup react hook form in auth login

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import Login from './_components/login'
+export const metadata = {
+  title: 'POS Cafe | Login',
+  description: 'Login to your POS Cafe account',
+}
+
+export default function LoginPage() {
+  return <Login />
+}
