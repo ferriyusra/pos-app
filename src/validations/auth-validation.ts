bec557190fe1dccd4d17cb066bcf74bbdced29ca
ferriyusra <ferisakuragi@gmail.com>
2025-07-25 15:01:18 +0700

    feat: refactor input component

--- src/validations/auth-validation.ts
+++ src/validations/auth-validation.ts
@@ -4,7 +4,7 @@ export const loginSchema = z.object({
 	email: z
 		.string()
 		.min(1, 'Email is required')
-		.email('Pkease enter a valid email address'),
+		.email('Please enter a valid email address'),
 	password: z.string().min(1, 'Password is required'),
 });
 
