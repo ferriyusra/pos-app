18aa569f9493ef21e1f3e0f30a3c46b11b2a8a18
ferriyusra <ferisakuragi@gmail.com>
2025-08-29 11:19:55 +0700

    feat: pagination and dropdown at usermanagement

--- src/app/(dashboard)/admin/user/_components/user.tsx
+++ src/app/(dashboard)/admin/user/_components/user.tsx
@@ -1,37 +1,81 @@
+/* eslint-disable react/jsx-key */
 'use client';
+
 import DataTable from '@/components/common/data-table';
+import DropdownAction from '@/components/common/dropdown-action';
 import { Button } from '@/components/ui/button';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { HEADER_TABLE_USER } from '@/constants/user-constant';
+import useDataTable from '@/hooks/use-data-table';
 import { createClient } from '@/lib/supabase/client';
-import { Dialog, DialogTrigger } from '@radix-ui/react-dialog';
 import { useQuery } from '@tanstack/react-query';
+import { Pencil, Trash2 } from 'lucide-react';
 import { useMemo } from 'react';
 import { toast } from 'sonner';
 
 export default function UserManagement() {
 	const supabase = createClient();
+
+	const { currentPage, currentLimit, handleChangePage, handleChangeLimit } =
+		useDataTable();
+
 	const { data: users, isLoading } = useQuery({
-		queryKey: ['users'],
+		queryKey: ['users', currentPage, currentLimit],
 		queryFn: async () => {
-			const { data, error } = await supabase
+			const result = await supabase
 				.from('profiles')
 				.select('*', { count: 'exact' })
+				.range((currentPage - 1) * currentLimit, currentPage * currentLimit - 1)
 				.order('created_at');
 
-			if (error)
+			if (result.error)
 				toast.error('Get User data failed', {
-					description: error.message,
+					description: result.error.message,
 				});
 
-			return data;
+			return result;
 		},
 	});
 
 	const filteredData = useMemo(() => {
-		return (users || []).map((user, index) => {
-			return [index + 1, user.id, user.name, user.role, ''];
+		return (users?.data || []).map((user, index) => {
+			return [
+				index + 1,
+				user.id,
+				user.name,
+				user.role,
+				<DropdownAction
+					menu={[
+						{
+							label: (
+								<span className='flex item-center gap-2'>
+									<Pencil />
+									Edit
+								</span>
+							),
+							action: () => {},
+						},
+						{
+							label: (
+								<span className='flex item-center gap-2'>
+									<Trash2 className='text-red-400' />
+									Delete
+								</span>
+							),
+							variant: 'destructive',
+							action: () => {},
+						},
+					]}
+				/>,
+			];
 		});
+	}, [users]);
+
+	const totalPages = useMemo(() => {
+		return users && users.count !== null
+			? Math.ceil(users.count / currentLimit)
+			: 0;
 	}, [users]);
 
 	return (
@@ -39,7 +83,7 @@ export default function UserManagement() {
 			<div className='flex flex-col lg:flex-row mb-4 gap-2 justify-between w-full'>
 				<h1 className='text-2xl font-bold'>User Management</h1>
 				<div className='flex gap-2'>
-					<Input placeholder='Search by name...' />
+					<Input placeholder='Search by name' />
 					<Dialog>
 						<DialogTrigger asChild>
 							<Button variant='outline'>Create</Button>
@@ -51,6 +95,11 @@ export default function UserManagement() {
 				header={HEADER_TABLE_USER}
 				data={filteredData}
 				isLoading={isLoading}
+				totalPages={totalPages}
+				currentPage={currentPage}
+				currentLimit={currentLimit}
+				onChangePage={handleChangePage}
+				onChangeLimit={handleChangeLimit}
 			/>
 		</div>
 	);
