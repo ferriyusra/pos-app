b77f3e5726de107fa4805f3ef9bb0db0c9e15d0a
ferriyusra <ferisakuragi@gmail.com>
2025-09-02 14:31:13 +0700

    feat: upload file avatar and refactor create user

--- src/app/(dashboard)/admin/user/action.ts
+++ src/app/(dashboard)/admin/user/action.ts
@@ -1,15 +1,17 @@
 'use server';
+
+import { uploadFile } from '@/actions/storage-action';
 import { createClient } from '@/lib/supabase/server';
 import { AuthFormState } from '@/types/auth';
 import { createUserSchema } from '@/validations/auth-validation';
 
 export async function createUser(prevState: AuthFormState, formData: FormData) {
-	const validatedFields = createUserSchema.safeParse({
+	let validatedFields = createUserSchema.safeParse({
 		email: formData.get('email'),
 		password: formData.get('password'),
 		name: formData.get('name'),
 		role: formData.get('role'),
-		// avatar_url: formData.get('avatar_url'),
+		avatar_url: formData.get('avatar_url'),
 	});
 
 	if (!validatedFields.success) {
@@ -22,7 +24,33 @@ export async function createUser(prevState: AuthFormState, formData: FormData) {
 		};
 	}
 
+	if (validatedFields.data.avatar_url instanceof File) {
+		const { errors, data } = await uploadFile(
+			'images',
+			'users',
+			validatedFields.data.avatar_url
+		);
+		if (errors) {
+			return {
+				status: 'error',
+				errors: {
+					...prevState.errors,
+					_form: [...errors._form],
+				},
+			};
+		}
+
+		validatedFields = {
+			...validatedFields,
+			data: {
+				...validatedFields.data,
+				avatar_url: data.url,
+			},
+		};
+	}
+
 	const supabase = await createClient();
+
 	const { error } = await supabase.auth.signUp({
 		email: validatedFields.data.email,
 		password: validatedFields.data.password,
@@ -30,7 +58,7 @@ export async function createUser(prevState: AuthFormState, formData: FormData) {
 			data: {
 				name: validatedFields.data.name,
 				role: validatedFields.data.role,
-				// avatar_url: validatedFields.data.avatar_url,
+				avatar_url: validatedFields.data.avatar_url,
 			},
 		},
 	});
